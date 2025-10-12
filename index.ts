f99226c27211057eb7d12f2d322a9f7e51617f44
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-10-12 22:27:44 +0200

    add figlet

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+import figlet from "figlet";
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-      return new Response("First Bun Http Server! Hot reload is enabled!");
+      return new Response(figlet.textSync("First Bun Http Server! Hot reload is enabled!", { font: "Standard" }));
     },
   });
   
