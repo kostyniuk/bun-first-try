8f70f0f598c6cf4ab7ab0902084119abfc07d61a
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-10-12 22:10:00 +0200

    add first http server & hot reload

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+      return new Response("First Bun Http Server! Hot reload is enabled!");
+    },
+  });
+  
+  console.log(`Listening on http://localhost:${server.port} ...`);
+  
