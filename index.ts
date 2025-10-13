037e68d9befa23c424d3e849b5b95611390f10f5
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-10-13 23:08:50 +0200

    add seed + api routes to display data from DB

--- index.ts
+++ index.ts
@@ -1,8 +1,38 @@
 import figlet from "figlet";
 
+import db from "./lib/db";
+import seed from "./lib/seed";
+
 const server = Bun.serve({
+    routes: {
+        "/": {
+            GET: (req) => {
+                return new Response(figlet.textSync("First Bun Http Server! Hot reload is enabled!", { font: "Standard" }));
+            }
+        },
+        "/seed": {
+            GET: async (req) => {
+                await seed();
+                return new Response("Seeded the database");
+            }
+        },
+        "/users": {
+            GET: async (req) => {
+                return new Response(JSON.stringify(db.query(`SELECT * FROM users`).all()));
+            }
+        },
+        "/posts": {
+            GET: async (req) => {
+                return new Response(JSON.stringify(db.query(`SELECT * FROM posts`).all()));
+            }
+        }
+    },
     port: 3000,
     fetch(req) {
+
+
+
+
       return new Response(figlet.textSync("First Bun Http Server! Hot reload is enabled!", { font: "Standard" }));
     },
   });
---
+++ lib/seed.ts
@@ -0,0 +1,11 @@
+import db from "./db";
+
+export default async function seed() {
+    await db.query(`
+    INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com')
+    `).run();
+
+    await db.query(`
+    INSERT INTO posts (title, content) VALUES ('Hello, world!', 'This is a test post.')
+    `).run();
+}   
